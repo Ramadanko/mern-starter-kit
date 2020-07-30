3579477ff5df710f14a5bfe557a6e0d5493ae378
Ramadanko <mohammed.ramadanko@gmail.com>
2020-07-30 14:51:26 +0200

    fix code styling using standard

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,28 +1,28 @@
 import bunyan from 'bunyan'
 
 const loggers = {
-    test: bunyan.createLogger({ name: 'development', level: 'fatal' }),
-    production: bunyan.createLogger({ name: 'development', level: 'info' }),
-    development: bunyan.createLogger({ name: 'development', level: 'debug' })
+  test: bunyan.createLogger({ name: 'development', level: 'fatal' }),
+  production: bunyan.createLogger({ name: 'development', level: 'info' }),
+  development: bunyan.createLogger({ name: 'development', level: 'debug' })
 }
 
 export default {
-    development: {
-        log: loggers.development,
-        database: {
-            dsn: process.env.DB_DSN_DEVELOPMENT
-        }
-    },
-    production: {
-        log: loggers.production,
-        database: {
-            dsn: process.env.DB_DSN_PRODUCTION
-        }
-    },
-    test: {
-        log: loggers.test,
-        database: {
-            dsn: process.env.DB_DSN_TEST
-        }
+  development: {
+    log: loggers.development,
+    database: {
+      dsn: process.env.DB_DSN_DEVELOPMENT
     }
+  },
+  production: {
+    log: loggers.production,
+    database: {
+      dsn: process.env.DB_DSN_PRODUCTION
+    }
+  },
+  test: {
+    log: loggers.test,
+    database: {
+      dsn: process.env.DB_DSN_TEST
+    }
+  }
 }
