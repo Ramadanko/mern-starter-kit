2f6267c0a50ccdc1fa5e2a9bb12bc41cbce4f21c
Ramadanko <mohammed.ramadanko@gmail.com>
2020-07-30 16:55:45 +0200

    register app routes and run standard fix command

--- backend/src/utilities/App/Middleware.js
+++ backend/src/utilities/App/Middleware.js
@@ -1,22 +1,21 @@
 import bodyParser from 'body-parser'
-import compression from 'compression';
-import helmet from 'helmet';
+import compression from 'compression'
+import helmet from 'helmet'
 import morgan from 'morgan'
 import cors from 'cors'
 
 export default (app) => {
   if (process.env.NODE_ENV !== 'production') {
-    app.use(cors());
+    app.use(cors())
   }
-  app.use(morgan('dev'));
-  app.use(helmet());
+  app.use(morgan('dev'))
+  app.use(helmet())
   app.use(helmet.hidePoweredBy())
   app.use(compression())
   /**
    * @description body parser
    * Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
    */
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
 }
