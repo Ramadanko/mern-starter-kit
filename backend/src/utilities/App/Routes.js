e55966fd1838c68babad866023b7f6fd36831c3c
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-01 18:29:41 +0200

    Add TaskRoute to application routes

--- backend/src/utilities/App/Routes.js
+++ backend/src/utilities/App/Routes.js
@@ -1,10 +1,12 @@
 import UserRoutes from '../../modules/User/UserRoute'
 import AuthRoute from '../../modules/Auth/AuthRoute'
 import { isLoggedIn, validateUserInDB } from '../../modules/Auth/AuthController'
+import TaskRoute from '../../modules/Task/TaskRoute'
 
 export default (app) => {
   app.use(AuthRoute)
   // secure API access
   app.all('/api/*', isLoggedIn, validateUserInDB)
   app.use('/api', UserRoutes)
+  app.use('/api', TaskRoute)
 }
