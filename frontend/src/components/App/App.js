d0a86cf3d2640c7ab2ec0aa0c67e2fd465bf55e7
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-03 16:14:08 +0200

    add react-helmet and Header components to App component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,32 +1,38 @@
-import loadable from '@loadable/component';
-import React from 'react';
+import loadable from '@loadable/component'
+import React from 'react'
 import Auth from '../../utilities/Auth/Auth'
 import { Route, Switch, Redirect, useLocation } from 'react-router-dom'
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { CssBaseline } from '@material-ui/core'
+import { Helmet } from 'react-helmet'
+import Header from '../../common/Header/Header'
 
 const fallback = <CircularProgress />
-const Home = loadable(() => import("../Home/Home"), { fallback });
-const PageNotFound = loadable(() => import("../PageNotFound/PageNotFound"))
+const Home = loadable(() => import('../Home/Home'), { fallback })
+const PageNotFound = loadable(() => import('../PageNotFound/PageNotFound'))
 
-function App() {
-  let location = useLocation();
-  debugger;
+function App () {
+  const location = useLocation()
   if (!Auth.isLoggedIn()) {
     Auth.setRedirectedUrl(location.pathname)
-    return <Redirect to="/login" />
+    return <Redirect to='/login' />
   }
   return (
     <>
+      <Helmet>
+        <title>Mern Starter Kit</title>
+        <meta name='description' content='A starter kit for building web apps using MongoDB, ExpressJS, ReactJS & NodeJS' />
+      </Helmet>
       <CssBaseline />
-      <div className="main-content">
+      <Header />
+      <div className='main-content'>
         <Switch>
-          <Route exact path="/"> <Home /></Route>
+          <Route exact path='/'> <Home /></Route>
           <Route><PageNotFound /></Route>
         </Switch>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
