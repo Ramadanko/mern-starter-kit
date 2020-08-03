4af6d1b16b3d37a77c91914e5dfe812dd5c1ea06
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-03 15:21:22 +0200

    Enhance Login component

---
+++ frontend/src/components/Login/LoginUI.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Box,
+  Typography,
+  Container,
+  LinearProgress
+} from '@material-ui/core/'
+import Alert from '@material-ui/lab/Alert'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  alert: {
+    width: '100%'
+  }
+}))
+
+function Copyright () {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://www.linkedin.com/in/mohammed-ramadan-85664898/' target='_blank'>
+        Ramadanko's starter kit for react
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+export default ({ errors, handleSubmit, handleEmail, handlePassword, saving, ...props }) => {
+  const classes = useStyles()
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        {errors.onSaving &&
+          <Alert variant='filled' severity='error' className={classes.alert}>
+            {errors.onSaving}
+          </Alert>}
+
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+            onChange={handleEmail}
+            error={!!errors.email}
+            helperText={errors.email}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+            onChange={handlePassword}
+            error={!!errors.password}
+            helperText={errors.password}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+            disabled={saving}
+          >
+            Sign In
+          </Button>
+          {saving && <LinearProgress />}
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
