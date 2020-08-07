4d2ce4194de528a7e223d99167f716e14f72db8f
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-07 18:32:09 +0200

    add front end form validation to TaskForm component

--- frontend/src/components/Task/TaskForm.js
+++ frontend/src/components/Task/TaskForm.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import * as taskActions from './TaskActions'
 import { useHistory } from 'react-router-dom'
 import TinyMCE from '../../common/TinyMCE/TinyMCE'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from 'redux'
 import {
   Button,
-  TextField,
   Select,
   OutlinedInput,
-  InputLabel,
-  FormHelperText,
   FormControl,
-  Typography,
   LinearProgress,
-  MenuItem
+  MenuItem,
+  FormHelperText
 } from '@material-ui/core/'
+import isEmpty from 'validator/es/lib/isEmpty'
+import sanitizeHtml from 'sanitize-html'
 
 const TaskForm = ({ taskStatusOptions, actions, ...props }) => {
 
-  let history = useHistory();
-  let [task, setTask] = useState({...props.task});
+  let history = useHistory()
+  const [errors, setErrors] = useState({})
+  const [saving, setSaving] = useState(false)
+  let [task, setTask] = useState({ ...props.task })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    if (!isFormValid()) {
+      return
+    }
+    setSaving(true)
     actions.saveTask(task)
       .then(task => {
         history.push(`/task/${task._id}`)
       })
       .catch(err => {
-        console.log('err', err);
-      });
+        console.log('err', err)
+      }).finally( () => setSaving(true))
   }
 
   const handleChange = (e) => {
-    let { name, value } = e.target;
-    setTask( prevTask => ({...prevTask, [name]: value}))
+    let { name, value } = e.target
+    setTask(prevTask => ({ ...prevTask, [name]: value }))
   }
 
   const handleRteChange = (value) => {
@@ -46,13 +51,42 @@ const TaskForm = ({ taskStatusOptions, actions, ...props }) => {
     }))
   }
 
+  const isFormValid = () => {
+    const errors = {}
+
+    if (!task.title && isEmpty(task.title)) {
+      errors.title = 'Title cannot be empty'
+    }
+
+    if( !validateEditor(task.description) ){
+      errors.description = "Invalid editor contents";
+    }
+
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  const validateEditor = (value) => {
+    return sanitizeHtml(value, {
+      exclusiveFilter: (frame) => {
+        return frame.tag === 'script'
+      },
+      textFilter: (value) => {
+        return value.replace(/\\n|\s\s/g, '').trim()
+      },
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h2']),
+      allowedAttributes: false
+    })
+  }
+
   return (
     <>
       <h1>{task._id ? 'Edit' : 'Create'} task</h1>
       <form onSubmit={handleSubmit} noValidate autoComplete="off" className="task-form">
-        <FormControl margin="dense" fullWidth>
+        <FormControl margin="dense" fullWidth error={!!errors.title}>
           <label htmlFor="title">Title</label>
-          <OutlinedInput required id='title' name='title' autoFocus onChange={handleChange} value={task.title} />
+          <OutlinedInput required id='title' name='title' autoFocus onChange={handleChange} value={task.title}/>
+          {errors.title ? <FormHelperText>{errors.title}</FormHelperText> : null}
         </FormControl>
         <FormControl fullWidth>
           <label htmlFor="status">Status</label>
@@ -62,10 +96,13 @@ const TaskForm = ({ taskStatusOptions, actions, ...props }) => {
             ))}
           </Select>
         </FormControl>
-        <FormControl fullWidth>
-          <TinyMCE handleRteChange={handleRteChange} value={task.description} />
+        <FormControl fullWidth error={!!errors.description}>
+          <TinyMCE handleRteChange={handleRteChange} value={task.description}/>
+          {errors.description ? <FormHelperText>{errors.description}</FormHelperText> : null}
         </FormControl>
-        <Button variant="contained" color="primary" type="submit">Save</Button>
+        <Button variant="contained" color="primary" type="submit" disabled={saving}>Save</Button>
+        <br/>
+        {saving ? <LinearProgress/> : null}
       </form>
     </>
   )
