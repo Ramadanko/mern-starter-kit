617058fc590cc87a7fbf85fbdc6b198403e5d5c3
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-09 21:28:12 +0200

    wire backend with frontend static routes for SPA

--- backend/src/App/Routes.js
+++ backend/src/App/Routes.js
@@ -2,6 +2,7 @@ import UserRoutes from '../modules/User/UserRoute'
 import AuthRoute from '../modules/Auth/AuthRoute'
 import { isLoggedIn, validateUserInDB } from '../modules/Auth/AuthController'
 import TaskRoute from '../modules/Task/TaskRoute'
+import path from 'path'
 
 export default (app) => {
   app.use('/api',AuthRoute)
@@ -9,4 +10,7 @@ export default (app) => {
   app.all('/api/*', isLoggedIn, validateUserInDB)
   app.use('/api', UserRoutes)
   app.use('/api', TaskRoute)
+  app.get('/*', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, '../../../frontend/build/index.html'))
+  });
 }
