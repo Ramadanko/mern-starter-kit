816c8fa6bdc1273c71395f63f153b1c51cb9957c
Ramadanko <mohammed.ramadanko@gmail.com>
2020-08-26 19:29:52 +0200

    create reusable filter function

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -7,3 +7,12 @@ export const buildObjectToSave = (body, keys) => {
   })
   return data
 }
+
+const sortByValues = ['createdAt', '-createdAt', 'title', '-title']
+
+export const defaultFilter = (query) => {
+  const page = query.page && parseInt(query.page) ? parseInt(query.page) - 1 : 0
+  const limit = query.limit && parseInt(query.limit) ? parseInt(query.limit) : 10
+  const sortBy = query.sortBy && sortByValues.find(query.sortBy) !== -1 ? query.sortBy : 'createdAt'
+  return { page, limit, sortBy }
+}
